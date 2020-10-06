d9ab959728ec5c1a50b3af0b3dce93320cf0036c
Mr-Personne <h.sacha07@gmail.com>
2020-10-06 13:30:31 +0000

    split & cleanup random word into list

--- hangman.py
+++ hangman.py
@@ -5,10 +5,15 @@ print('Hangman game start')
 tries = 6
 randIndex = random.randint(0, 267750)
 
+# open and get a random word from the text file
 text_file = open('sowpods.txt', 'r')
 wordlist = text_file.readlines()
 randWord = wordlist[randIndex]
+# print('word rand == ', randWord)
 
-
-print('word rand == ', randWord)
-
+# split the word into individual characters and make a list out of them
+splitWord = list(randWord)
+# print('split word == ', splitWord)
+# will remove last element of a list (the empty space, in this case)
+splitWord.pop()
+print('split word == ', splitWord)
