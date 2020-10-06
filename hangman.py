ff7f71cc56007e02bf904330dd5474702187af0f
Mr-Personne <h.sacha07@gmail.com>
2020-10-06 14:40:11 +0000

    pulled 1.0 and cleaned up prints in com

--- hangman.py
+++ hangman.py
@@ -7,17 +7,13 @@ print('Hangman game start')
 text_file = open('sowpods.txt', 'r')
 wordlist = text_file.readlines()
 wordlistLen = len(wordlist) - 1
-# print('list len ==', wordlistLen)
 randIndex = random.randint(0, wordlistLen)
 randWord = wordlist[randIndex]
-# print('word rand == ', randWord)
 
 # split the word into individual characters and make a list out of them
 splitWord = list(randWord)
-# print('split word == ', splitWord)
 # will remove last element of a list (the empty space, in this case)
 splitWord.pop()
-# print('split word == ', splitWord)
 
 #setup user answer list
 userAnswer = []
@@ -57,7 +53,6 @@ while tries > 0:
     userLetter = userLetter.capitalize()
 
     tries = checkIfCorrect(userLetter, tries)
-    # print('tries test = ', tries)
     #check if user found all letters and wins
     if correct == len(splitWord):
         print('Correct answer == ', splitWord)
